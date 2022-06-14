4df0bfd55399ed83ee69b6330d8c6dc5730b6724
leomascara <leonardomascara@gmail.com>
2022-06-14 19:37:58 +0100

    Dockerizando a App

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_wtf.csrf import CSRFProtect
+import os
 
 app = Flask(__name__)
 
@@ -7,8 +8,9 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Olá Mundo, por Leonardo Martins Scaramel da FIAP 7ASO"
+    return "Olá Mundo, por Leonardo Martins Scaramel da FIAP 7ASO - V2"
 
 
 if __name__ == '__main__':
-    app.run()
+    port = os.getenv('PORT')
+    app.run('0.0.0.0', port=port)
