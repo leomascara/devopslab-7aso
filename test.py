6ec14d9ac208ce7d5a7cc9b649bdd9a5d0561a3e
leomascara <leonardomascara@gmail.com>
2022-06-25 16:13:12 +0100

    Rollback

--- test.py
+++ test.py
@@ -16,4 +16,4 @@ class Test(unittest.TestCase):
 
     def test_conteudo(self):
         # verifica o retorno do conteudo da pagina
-        self.assertEqual(self.result.data.decode('utf-8'), "Olá Mundo, por Leonardo Martins Scaramel da FIAP 7ASO - Final Version")
+        self.assertEqual(self.result.data.decode('utf-8'), "Olá Mundo, por Leonardo Martins Scaramel da FIAP 7ASO - V2.1")
